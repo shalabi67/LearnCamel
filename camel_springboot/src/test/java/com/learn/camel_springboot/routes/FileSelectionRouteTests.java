0006f0cbe843cbed31612c2a456a813df5c449b0
mohammad <shalabi67@hotmail.com>
2018-04-01 06:40:14 +0200

    testing working

---
+++ camel_springboot/src/test/java/com/learn/camel_springboot/routes/FileSelectionRouteTests.java
@@ -0,0 +1,84 @@
+package com.learn.camel_springboot.routes;
+
+import com.learn.camel_springboot.camel.routes.FileSelectionRoute;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = FileSelectionRouteTests.class)
+@ActiveProfiles("test")
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.learn.camel_springboot.camel.routes"})
+public class FileSelectionRouteTests {
+
+    public static final String NICE_DOG = "nice dog", NASTY_CAT="nasty cat", SUPERNASTY_CAT="super nasty cat";
+
+    @EndpointInject(uri = "{{dogEndpoint}}")
+    protected MockEndpoint dogEndpoint;
+
+    @EndpointInject(uri = "{{catEndpoint}}")
+    protected MockEndpoint catEndpoint;
+
+
+    @EndpointInject(uri = "{{animalSource}}")
+    protected ProducerTemplate animalSource;
+
+    @Test
+    @DirtiesContext
+    public void testDog() throws Exception {
+
+        animalSource.sendBodyAndHeader("test", FileSelectionRoute.CAMEL_FILE_NAME, NICE_DOG);
+
+
+        dogEndpoint.message(0).predicate(m -> {
+            String header = m.getIn().getHeader(FileSelectionRoute.CAMEL_FILE_NAME).toString();
+            return NICE_DOG.equals(header);
+        });
+        dogEndpoint.expectedMessageCount(1);
+        dogEndpoint.assertIsSatisfied();
+
+        catEndpoint.expectedMessageCount(0);
+        catEndpoint.assertIsSatisfied();
+    }
+
+    @Test
+    @DirtiesContext
+    public void testCat() throws Exception {
+
+
+        animalSource.sendBodyAndHeader("test", FileSelectionRoute.CAMEL_FILE_NAME, NASTY_CAT);
+        animalSource.sendBodyAndHeader("test", FileSelectionRoute.CAMEL_FILE_NAME, SUPERNASTY_CAT);
+
+
+        catEndpoint.message(0).predicate(m -> {
+            String header = m.getIn().getHeader(FileSelectionRoute.CAMEL_FILE_NAME).toString();
+            return NASTY_CAT.equals(header);
+        });
+
+        catEndpoint.message(1).predicate(m -> {
+            String header = m.getIn().getHeader(FileSelectionRoute.CAMEL_FILE_NAME).toString();
+            return SUPERNASTY_CAT.equals(header);
+        });
+
+        catEndpoint.expectedMessageCount(2);
+
+        dogEndpoint.expectedMessageCount(0);
+
+        dogEndpoint.assertIsSatisfied();
+
+        catEndpoint.assertIsSatisfied();
+
+
+
+    }
+
+}
