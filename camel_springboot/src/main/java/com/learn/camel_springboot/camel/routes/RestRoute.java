bf7761372f1d765ca747665b4db5844ed7f60d6e
mohammad <shalabi67@hotmail.com>
2018-04-02 07:52:10 +0200

    adding unit testing.

--- camel_springboot/src/main/java/com/learn/camel_springboot/camel/routes/RestRoute.java
+++ camel_springboot/src/main/java/com/learn/camel_springboot/camel/routes/RestRoute.java
@@ -3,6 +3,7 @@ package com.learn.camel_springboot.camel.routes;
 import org.apache.camel.Exchange;
 import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.model.RouteDefinition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -11,14 +12,17 @@ import org.springframework.stereotype.Component;
  * read messages from BAGS_ORDERS queue and post them to http://localhost:7777/ http service.
  */
 @Component
-public class RestRoute extends RouteBuilder{
+public class RestRoute extends RouteBuilder  {
+    public static final String JSON_APPLICATION = "application/json";
+    public static final String REST_ROUT_ID = "REST_ROUT";
 
     @Override
     public void configure() throws Exception {
         from("{{bagsOrderSource}}")
+                .routeId(REST_ROUT_ID)
                 .log("send to rest service")
-                .setHeader("CamelHttpMethod", constant("POST"))
-                .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+                .setHeader(Exchange.HTTP_METHOD, HttpMethods.POST)
+                .setHeader(Exchange.CONTENT_TYPE, constant(JSON_APPLICATION))
                 .to("{{restEndPoint}}");
     }
 }
--- camel_springboot/src/test/java/com/learn/camel_springboot/routes/RestRouteIntegrationTests.java
+++ camel_springboot/src/test/java/com/learn/camel_springboot/routes/RestRouteIntegrationTests.java
@@ -3,14 +3,20 @@ package com.learn.camel_springboot.routes;
 import com.learn.camel_springboot.Facade.OrderAdapter;
 import com.learn.camel_springboot.camel.routes.RestRoute;
 import com.learn.camel_springboot.models.Order;
+import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.component.http4.HttpMethods;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.RouteDefinition;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,42 +24,23 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
-import java.util.Map;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = RestRouteTests.class)
+@SpringBootTest(classes = RestRouteIntegrationTests.class)
 @ActiveProfiles("test")
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.learn.camel_springboot.camel.routes"})
-public class RestRouteTests {
+public class RestRouteIntegrationTests {
 
     @EndpointInject(uri = "{{bagsOrderSource}}")
     ProducerTemplate ordersQueueProducer;
 
-    @EndpointInject(uri = "{{restEndPoint}}")
-    MockEndpoint restMock;
-
-
     @Test
-    public void testRoute() throws Exception {
-        Assert.assertNotNull(restMock);
-        Assert.assertNotNull(ordersQueueProducer);
-        int messageCount = 1;
-
-        restMock.expectedMessageCount(messageCount);
-        restMock.expectedBodiesReceived(order.toString());
-
+    public void shouldSucceed() throws Exception {
         ordersQueueProducer.sendBody(order.toString());
-        List<Exchange> list = restMock.getReceivedExchanges();
-
-        Assert.assertEquals("Expected one exchange", messageCount, list.size());
-        Map<String, Object> header =  list.get(0).getIn().getHeaders();
-        Assert.assertEquals("expected POST method", HttpMethods.POST.name(), header.get(Exchange.HTTP_METHOD));
-        Assert.assertEquals("expected json content type", RestRoute.JSON_APPLICATION, header.get(Exchange.CONTENT_TYPE));
-
-        restMock.assertIsSatisfied();
     }
 
     private Order order = OrderAdapter.createOrder();
+
 }
